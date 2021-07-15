f1ea44f751b67b2a4dd17a97cbabfed5c447df1d
mster <michaelsterpka@gmail.com>
2021-07-14 19:54:25 -0700

    Drop MoshMenu styles

--- src/components/MoshMenu/index.js
+++ src/components/MoshMenu/index.js
@@ -5,9 +5,6 @@ import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 
-/* styling */
-import "./mosh.css";
-
 const MoshMenu = (props) => {
   const { modes, onClickUpload, isLoading, onFileChange } = props;
 
